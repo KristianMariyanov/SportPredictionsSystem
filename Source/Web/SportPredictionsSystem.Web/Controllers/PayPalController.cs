1bb4912ee170d2cdbe7f037a54105d7c8bd7284b
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-09-30 18:39:34 +0300

    Start implementing paypal payments #13

---
+++ Source/Web/SportPredictionsSystem.Web/Controllers/PayPalController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SportPredictionsSystem.Web.Controllers
+{
+    public class PayPalController : Controller
+    {
+        // GET: PayPal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/Web/SportPredictionsSystem.Web/Models/PayPal/PayPalModel.cs
@@ -0,0 +1,41 @@
+﻿namespace SportPredictionsSystem.Web.Models.PayPal
+{
+    using System.Configuration;
+
+    public class PayPalModel
+    {
+        public string Cmd { get; set; }
+
+        public string Business { get; set; }
+
+        public string NoShipping { get; set; }
+
+        public string Return { get; set; }
+
+        public string CancelReturn { get; set; }
+
+        public string NotifyUrl { get; set; }
+
+        public string CurrencyCode { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string Amount { get; set; }
+
+        public string ActionUrl { get; set; }
+
+        public PayPalModel(bool useSandbox)
+        {
+            this.Cmd = "_xclick";
+            this.Business = ConfigurationManager.AppSettings["this.Business"];
+            this.CancelReturn = ConfigurationManager.AppSettings["this.CancelReturn"];
+            this.Return = ConfigurationManager.AppSettings["return"];
+            this.ActionUrl = useSandbox ? ConfigurationManager.AppSettings["test_url"] : ConfigurationManager.AppSettings["Prod_url"];
+
+            // We can add parameters here, for example OrderId, CustomerId, etc….
+            this.NotifyUrl = ConfigurationManager.AppSettings["this.NotifyUrl"];
+            // We can add parameters here, for example OrderId, CustomerId, etc….
+            this.CurrencyCode = ConfigurationManager.AppSettings["this.CurrencyCode"];
+        }
+    }
+}
